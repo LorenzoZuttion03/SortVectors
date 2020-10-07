4e012a0bdbc61dcb51f7c88719b9f7b980bbadc4
Lorenzo Zuttion <LORENZO.ZUTTION@local.ittcesena.it>
2020-10-07 11:06:44 +0200

    First step

---
+++ SortVectors/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SortVectors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vet = new int[10] { 4, -1, 100, 85, 4, 6, 77, 35, 2, 0 };
+            for(int i = 0; i<10; i++)
+            {
+                Console.WriteLine(vet[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
