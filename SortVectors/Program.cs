bf69acaf8cb5aba022e179a88b69f89476b6c673
Lorenzo Zuttion <LORENZO.ZUTTION@local.ittcesena.it>
2020-10-07 11:48:35 +0200

    Second Step : Ordinamento

--- SortVectors/Program.cs
+++ SortVectors/Program.cs
@@ -7,11 +7,17 @@ namespace SortVectors
         static void Main(string[] args)
         {
             int[] vet = new int[10] { 4, -1, 100, 85, 4, 6, 77, 35, 2, 0 };
+            Ordina(vet);
             for(int i = 0; i<10; i++)
             {
                 Console.WriteLine(vet[i]);
             }
+
             Console.ReadKey();
+        }
+        static void Ordina(int[] v)
+        {
+            Array.Sort(v);
         }
     }
 }
