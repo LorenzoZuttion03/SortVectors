d3a729ebc655c9beec5542017577d9a1bfbf2ae5
lorenzo.fabbri.4g <lorenzo.fabbri.4g@PC7307>
2020-10-21 11:23:05 +0200

    Inserito il metodo per il calcolo del minimo

--- SortVectors/Program.cs
+++ SortVectors/Program.cs
@@ -7,7 +7,12 @@ namespace SortVectors
         static void Main(string[] args)
         {
             int[] vet = new int[10] { 4, -1, 100, 85, 4, 6, 77, 35, 2, 0 };
+
             Ordina(vet);
+
+            int min = Minimo(vet);
+            Console.WriteLine("Il valore minimo è: " + min);
+
             for(int i = 0; i<10; i++)
             {
                 Console.WriteLine(vet[i]);
@@ -18,6 +23,11 @@ namespace SortVectors
         static void Ordina(int[] v)
         {
             Array.Sort(v);
+        }
+
+        static int Minimo(int[] v)
+        {
+            return v[0];
         }
     }
 }
